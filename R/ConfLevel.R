0c435a41f6e22b9c54359bcfaaf5d046473284ea
Qian Zhao <qzhao1@stanford.edu>
2022-10-25 11:55:36 -0700

    add new functions

---
+++ R/ConfLevel.R
@@ -0,0 +1,44 @@
+#' Set confidence level to each depth
+#'
+#' Computes the confidence level at every level of the tree.
+#'
+#' If \code{method = Bonferroni}, the confidence level at depth d is
+#' \deqn{
+#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, D\geq D_\min,
+#' }
+#' where \eqn{d_\max} is the maximum depth of the tree, \eqn{d_\min} is the smallest depth where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
+#'
+#' If \code{method = weighted_bonferroni}, the confidence level at depth d is
+#' \deqn{
+#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d + 2) n_d \sum_{B=2}^{d_\max - d_\min + 2}1/B},D\geq D_\min,
+#' }
+#' that is, the leaves get weights alpha/2 etc.
+#'
+#' @param nd A vector. Number of bounded regions at every level.
+#' @param alpha Numeric. confidence level.
+#' @param method Use \code{bonferroni} or \code{weighted_bonferroni} method to adjust for multiple hypothesis testing.
+#' @param n Number of observations.
+#' @param d Numeric value of the number of dimensions of the obs.
+#' @return A vector of confidence level at every depth (starting at depth = 1)
+#' @export
+ConfLevel <- function(nd, alpha, method, n, d){
+  Dmax <- length(nd)  # depth of tree
+  Dmin <- min(which(!is.na(nd)))
+  # set sig. level to each depth
+  ahat <- numeric(Dmax)
+  for(l in Dmin:Dmax){
+    switch (method,
+            "bonferroni" = {ahat[l] <- alpha / (Dmax - Dmin + 1) / nd[l]},
+            "weighted_bonferroni" = {
+              wsum <- sum(1/(2:(Dmax-Dmin + 2)))
+              ahat[l] <- alpha / (Dmax - l + 2) / nd[l] / wsum
+            }
+    )
+  }
+  if(Dmin>1) {  # dummy value: first few will not be used as there are no bounded regions
+    ahat[1:(Dmin-1)] <- 0.5
+  }
+
+  return(ahat)
+}
+
---
+++ R/SetBounds.R
@@ -0,0 +1,39 @@
+#' Add confidence bounds
+#'
+#' Recursively set confidence bounds at bounded nodes in the k-d tree.
+#'
+#' The upper and lower confidence bounds of a bounded region at depth \eqn{D} is given by
+#' \deqn{
+#' \text{lower} = \frac{qBeta(\hat{\alpha}_D/2, N+1, N-n)}{|R|}\\
+#' \text{upper} = \frac{qBeta(1-\hat{\alpha}_D/2, N+1, N-n)}{|R|}
+#' }
+#' where \eqn{\hat{\alpha}_D} is the significance level at depth \eqn{D}, \eqn{N} is the total number of obs.,  \eqn{n} is the number of obs. in this region,
+#' and |R| is the volume of the region. If there are no children nodes, ignore the lower and upper bounds of the children.
+#' We set the lower and upper bound of each region as
+#' \deqn{
+#' \widetilde{\text{lower}} = \max\left(\text{lower}, \text{lower}(\text{2nd child})\right) \\
+#' \widetilde{\text{upper}} = \max\left(\text{upper}, \text{upper}(\text{1st child}), \text{upper}(\text{2nd child})\right),
+#' }
+#' so that when we select nodes, we pick the largest nodes such that the empirical density is between \eqn{\widetilde{\text{lower}}} and  \eqn{\widetilde{\text{upper}}}.
+#' The confidence bounds are computed in the function \code{ConfLevel}.
+#' @param node A list element representing the region.
+#' @param ahat A vector of confidence levels at each depth, starting at depth = 1. Output of the funtion \code{ConfLevel}
+#' @param n Total number of observations.
+#' @return An updated k-d tree starting at input node.
+#' @export
+SetBounds <- function(node, ahat, n){
+  if (node$bounded){
+    lower <- stats::qbeta(ahat[node$depth] / 2, node$ndat + 1, n - node$ndat) / prod(node$up - node$low)
+    upper <- stats::qbeta(1 - ahat[node$depth] / 2, node$ndat + 1, n - node$ndat) / prod(node$up - node$low)
+  }
+  if(node$leaf){if (node$bounded) {node$lower <- lower; node$upper <- upper}
+  }else{
+    node$leftchild <- SetBounds(node$leftchild, ahat, n)
+    node$rightchild <- SetBounds(node$rightchild, ahat, n)
+  }
+  if(!node$leaf & node$bounded){
+    node$lower <- max(lower, node$leftchild$lower,node$rightchild$lower)
+    node$upper <- min(upper, node$leftchild$upper, node$rightchild$upper)
+  }
+  return(node)
+}
