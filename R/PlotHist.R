e02a1d28d5062bcff1983e83dd2a6ce11edc1f2a
Qian Zhao <qzhao1@stanford.edu>
2023-07-25 17:26:50 -0700

    change the colors in the histogram let user input their own color schemes set the midpoint of the color to correspond to the median of the empirical density in all of the boxes

--- R/PlotHist.R
+++ R/PlotHist.R
@@ -8,25 +8,31 @@
 #' @param X data matrix of size n by 2.
 #' @param B matrix of rectangles returned by the function \link[BetaTrees]{SelectNodes} or \link[BetaTrees]{BuildHist}.
 #' @param nsample maximum number of data points to display in the plot.
-#'
+#' @param ... additional parameter for the graph: \code{low}, \code{mid}, \code{high} specifies the colors in \link[ggplot2]{scale_fill_gradient2} function.
 #' @return A \code{ggplot} object.
 #' @export
 #' @examples
 #' X <-  matrix(rnorm(2000, 0, 1), ncol = 2)
 #' rect <- BuildHist(X, plot = FALSE)
 #' PlotHist(X, rect)
-PlotHist <- function(X, B, nsample = 10000){
+PlotHist <- function(X, B, nsample = 10000, ...){
   n <- nrow(X)
   boundary <- data.frame(
-    xmin = B[,1], xmax = B[,3], ymin = B[,2],ymax = B[,4],n = B[,8],
-    density = B[,5]
+    xmin = B[,1], xmax = B[,3], ymin = B[,2],ymax = B[,4],
+    nobs = B[,8], density = B[,5]
   )
-  # take a subsample if N is large
-  if(n > nsample){ x_small <- X[sample(1:n,  nsample, replace = FALSE),]
+  # take a subsample if n is large
+  if(n > nsample){ x_small <- X[sample(1:n,  nsample, replace = FALSE), ]
   }else{ x_small <- X}
+
+  # User can input the two colors in the color gradients of the histogram
+  params <-  list(...)
+  if(is.null(params$low)){low <- "#f7fbff"}
+  if(is.null(params$mid)){ mid <- "#6baed6"}
+  if(is.null(params$high)){ high <- "#08306b"}
   g <- ggplot2::ggplot() +
     ggplot2::geom_rect(ggplot2::aes(xmin = xmin, xmax = xmax,ymin = ymin, ymax = ymax,fill = density), color = "black",data = boundary)  +
-    ggplot2::scale_fill_gradient2(low = "#e0f3db", mid = "#a8ddb5", high = "#43a2ca",aesthetics = "fill") +
+    ggplot2::scale_fill_gradient2(low = low, mid = mid, high = high, midpoint = median(boundary$density), aesthetics = "fill") +
     ggplot2::geom_point(ggplot2::aes(x = x_small[,1], y = x_small[,2]), size = 0.4, alpha =n^(-0.25)) +
     ggplot2::xlab("X") +  ggplot2::ylab("Y") +
     ggplot2::theme_bw() +  ggplot2::theme(text=ggplot2::element_text(size =18))
