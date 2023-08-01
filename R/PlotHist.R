d78bb173dfeb1f4305168896a8a0a99ebe172ba3
Qian Zhao <qzhao1@stanford.edu>
2023-08-01 09:40:01 -0700

    update documentation; change xmin etc to boundary$xmin

--- R/PlotHist.R
+++ R/PlotHist.R
@@ -1,15 +1,17 @@
-#' Plot a 2D Beta Tree histogram
+#' Plot a 2D Beta tree histogram
 #'
-#' Draw a Beta tree histogram from the rectangles returned by \code{SelectNodes}.
+#' Draw a Beta tree histogram.
 #'
 #' \code{PlotHist} plots the histogram superimposed on the original dataset.
-#' If the number of data points exceeds \code{nsample}, randomly select \code{nsample} obvervations to show in the graph. Default value is \eqn{n_sample=10^4}
+#' If the number of data points exceeds \code{nsample}, randomly select \code{nsample} obs. to show in the graph. Default value is \eqn{n_sample=10^4}
 #'
 #' @param X data matrix of size n by 2.
 #' @param B matrix of rectangles returned by the function \link[BetaTrees]{SelectNodes} or \link[BetaTrees]{BuildHist}.
 #' @param nsample maximum number of data points to display in the plot.
 #' @param ... additional parameter for the graph: \code{low}, \code{mid}, \code{high} specifies the colors in \link[ggplot2]{scale_fill_gradient2} function.
 #' @return A \code{ggplot} object.
+#' @importFrom stats median
+#' @importFrom ggplot2 ggplot
 #' @export
 #' @examples
 #' X <-  matrix(rnorm(2000, 0, 1), ncol = 2)
@@ -31,11 +33,11 @@ PlotHist <- function(X, B, nsample = 10000, ...){
   if(is.null(params$mid)){ mid <- "#6baed6"}
   if(is.null(params$high)){ high <- "#08306b"}
   g <- ggplot2::ggplot() +
-    ggplot2::geom_rect(ggplot2::aes(xmin = xmin, xmax = xmax,ymin = ymin, ymax = ymax,fill = density), color = "black",data = boundary)  +
-    ggplot2::scale_fill_gradient2(low = low, mid = mid, high = high, midpoint = median(boundary$density), aesthetics = "fill") +
+    ggplot2::geom_rect(ggplot2::aes(xmin = boundary$xmin, xmax = boundary$xmax,ymin = boundary$ymin, ymax = boundary$ymax, fill = boundary$density), color = "black")  +
+    ggplot2::scale_fill_gradient2(low = low, mid = mid, high = high, midpoint = stats::median(boundary$density), aesthetics = "fill") +
     ggplot2::geom_point(ggplot2::aes(x = x_small[,1], y = x_small[,2]), size = 0.4, alpha =n^(-0.25)) +
-    ggplot2::xlab("X") +  ggplot2::ylab("Y") +
-    ggplot2::theme_bw() +  ggplot2::theme(text=ggplot2::element_text(size =18))
+    ggplot2::xlab("x") +  ggplot2::ylab("y") +
+    ggplot2::theme(text=ggplot2::element_text(size =18))
 
   return(g)
 }
