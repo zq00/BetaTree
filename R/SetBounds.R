59951c64433f87aa55b819c9403c36b1eb7cd256
Qian Zhao <qzhao1@stanford.edu>
2023-08-01 09:38:16 -0700

    Update description and add packages (i need for the vignettes) to suggest

--- R/SetBounds.R
+++ R/SetBounds.R
@@ -1,25 +1,33 @@
-#' Add confidence bounds
+#' Add confidence bounds to bounded region in the k-d tree
 #'
-#' Recursively set confidence bounds at bounded nodes in the k-d tree.
+#' Recursively set confidence bounds for bounded nodes in the k-d tree.
 #'
-#' The upper and lower confidence bounds of a bounded region at depth \eqn{D} is given by
+#' Because
 #' \deqn{
-#' \text{lower} = \frac{qBeta(\hat{\alpha}_D/2, N+1, N-n)}{|R|}\\
-#' \text{upper} = \frac{qBeta(1-\hat{\alpha}_D/2, N+1, N-n)}{|R|}
+#' F(R_k)\sim \mathrm{Binomial}(n_k+1,n-n_k),
 #' }
-#' where \eqn{\hat{\alpha}_D} is the significance level at depth \eqn{D}, \eqn{N} is the total number of obs.,  \eqn{n} is the number of obs. in this region,
-#' and |R| is the volume of the region. If there are no children nodes, ignore the lower and upper bounds of the children.
+#' where \eqn{R_k} is a region, \eqn{n} is the total number of obs. and \eqn{n_k} is the number of obs. in \eqn{R_k},
+#' the upper and lower confidence bounds of the average density in a bounded region at depth \eqn{D} is given by
+#' \deqn{
+#' \text{lower}(R_k) = \frac{qBeta(\hat{\alpha}_D/2, n_k+1, n-n_k)}{|R_k|}\\
+#' \text{upper}(R_k) = \frac{qBeta(1-\hat{\alpha}_D/2, n_k+1, n-n_k)}{|R_k|}
+#' }
+#' where \eqn{\hat{\alpha}_D} is the significance level at depth \eqn{D},
+#' and \eqn{|R_k|} is the volume of the region.
+#'
+#' We select nodes in which data are approximately uniformly distributed, i.e., when the empirical density falls within the confidence intervals of all of its children.
 #' We set the lower and upper bound of each region as
 #' \deqn{
-#' \widetilde{\text{lower}} = \max\left(\text{lower}, \text{lower}(\text{2nd child})\right) \\
+#' \widetilde{\text{lower}} = \max\left(\text{lower}, \text{lower}(\text{1st child}), \text{lower}(\text{2nd child})\right) \\
 #' \widetilde{\text{upper}} = \max\left(\text{upper}, \text{upper}(\text{1st child}), \text{upper}(\text{2nd child})\right),
 #' }
 #' so that when we select nodes, we pick the largest nodes such that the empirical density is between \eqn{\widetilde{\text{lower}}} and  \eqn{\widetilde{\text{upper}}}.
-#' The confidence bounds are computed in the function \code{ConfLevel}.
-#' @param node A list element representing the region.
-#' @param ahat A vector of confidence levels at each depth, starting at depth = 1. Output of the funtion \code{ConfLevel}
+#' The significance levels are computed with the function \link[BetaTrees]{ConfLevel}.
+#'
+#' @param node A list storing information about a region.
+#' @param ahat A vector of significance level at each depth, starting at depth = 0. Output of the funtion \code{ConfLevel}.
 #' @param n Total number of observations.
-#' @return An updated k-d tree starting at input node.
+#' @return An updated k-d tree starting at input node with \code{lower} and \code{upper} filled in.
 #' @export
 SetBounds <- function(node, ahat, n){
   if (node$bounded){
