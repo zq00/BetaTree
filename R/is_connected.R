40573e80cbf72e227400121a22c80cf9732567e5
Qian Zhao <qzhao1@stanford.edu>
2023-08-01 09:40:34 -0700

    update documentation

--- R/is_connected.R
+++ R/is_connected.R
@@ -1,17 +1,17 @@
 #' Are two regions distinct modes?
 #'
 #' Two regions \code{i} and \code{j} are two *distinct* modes if
-#' there *exists* one path connecting \code{i} and \code{j}, there exists at least one \code{R} along the path,
-#' such that the upper endpoint of confidence interval of \code{R} is *below* the lower endpoints of that of
+#' for *every* path connecting \code{i} and \code{j}, there exists at least one region \code{R} along the path,
+#' such that the upper endpoint of confidence interval ofthe average density of \code{R} is *below* the lower endpoints of that of
 #' both \code{i} and \code{j}.
 #'
 #' For computational reasons, we only check paths whose length is at most \code{cutoff}.
 #'
 #' @param i,j Test if the \eqn{i}-th and the \eqn{j}-th regions are distinct modes or not.
-#' @param g A graph returned by \link[igraph]{graph_from_adjacency_matrix}
+#' @param g A graph from the adjacency matrix returned by \link[igraph]{graph_from_adjacency_matrix}.
 #' @param ci A matrix of size N*2 of the confidence intervals of empirical densities.
-#'   Each row represents one region in the Beta Tree and the two columns correspond to the lower and upper confidence limits.
-#' @inherit FindModes
+#'   Each row stores the lower and upper confidence limits of one region in the Beta tree.
+#' @inheritParams FindModes
 #' @returns \code{unconnected} if the two regions are distinct modes and \code{connected} otherwise.
 #' @importFrom igraph all_simple_paths
 #' @export
