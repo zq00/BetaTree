b669ff3611cd7aa7cefa0a2646e010f1f280eeac
Qian Zhao <qzhao1@stanford.edu>
2023-08-01 09:40:28 -0700

    update documentation

--- R/find_mode.R
+++ R/find_mode.R
@@ -1,34 +1,35 @@
-#' Compute modes in the Beta Tree histogram
+#' Identifies distinct modes in Beta trees histogram
 #'
-#' Identifies distinct modes in the histogram
+#' Compute modes in the histogram.
 #'
-#' Two regions \code{i} and \code{j} are distinct modes if along at least one path connecting them,
+#' Two regions \code{i} and \code{j} are distinct modes if along every path connecting them,
 #'   there exists one region whose upper confidence limit is below the lower confidence limit
 #'   of both \code{i} and \code{j}.
-#' This function iterates through all of the regions in the Beta Tree histogram according to the
-#'   descending order of empirical density and marks a region as a distinct mode if it is a distinct mode
-#'   compared to every current modes.
+#' This function iterates through all the regions in the Beta tree histogram in
+#'   descending order of empirical density and marks a region as a mode if it is a distinct mode
+#'   from current modes.
 #'
 #' @param hist A matrix. Each row represents one region in the histogram.
 #'   If the histogram is constructed for \code{d} dimensional data, then the first \code{d} columns contain the lower bound of each dimension,
 #'   and the next \code{d} columns contain the upper bound. The \code{2d + 1} column contains the empirical density.
-#'    \code{hist} can be the output of \link[BetaTrees]{BuildHist} function.
+#'    \code{hist} can be the output of \link[BetaTrees]{BuildHist} or \link[BetaTrees]{SelectNodes} function.
 #' @param d A numeric value of the data dimension.
 #' @param cutoff Maximum path length. By default, we only evaluate paths with length at most \code{cutoff = 6}.
 #' @returns
 #' \describe{
 #' \item{mode}{A vector of candidate modes.}
-#' \item{hist}{A matrix of the input Beta Tree histogram.}
+#' \item{hist}{The input Beta tree histogram.}
 #' \item{g}{A graph object constructed using the adjacency matrix of the regions in the histogram.}
 #' }
 #' @export
 #' @importFrom igraph graph_from_adjacency_matrix
 #' @examples
 #' # A mixture of two Gaussians
-#' X <-  matrix(rnorm(4000, 0, 1), ncol = 2)
-#' X[1:1000, 1] <- X[1:1000, 1] + 4
+#' X <-  matrix(rnorm(6000, 0, 1), ncol = 2)
+#' X[1:1500, 1] <- X[1:1500, 1] + 4
 #' rect <- BuildHist(X) # construct a Beta Tree histogram
 #' modes <- FindModes(hist = rect, d = 2)$mode
+#' rect[modes, ] # show information of the modes
 
 FindModes <- function(hist, d, cutoff = 6){
   density <- hist[,(2*d+1)] # empirical density of each region
