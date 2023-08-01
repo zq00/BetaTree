dcf698c576995402d6aa5bddc13c962cb5dacd0b
Qian Zhao <qzhao1@stanford.edu>
2023-08-01 09:39:33 -0700

    update documentation; set the significance level of unbounded regions to be 0

--- R/ConfLevel.R
+++ R/ConfLevel.R
@@ -1,23 +1,23 @@
-#' Set confidence level to each depth
+#' Set confidence level for regions at each tree depth
 #'
-#' Computes the confidence level at every level of the tree.
+#' Computes the confidence level at each tree depth.
 #'
-#' If \code{method = Bonferroni}, the confidence level at depth d is
+#' If \code{method = "bonferroni"}, the significance level at depth d is
 #' \deqn{
-#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, D\geq D_\min,
+#' \hat{\alpha}_d =  \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, D\geq D_\min,
 #' }
-#' where \eqn{d_\max} is the maximum depth of the tree, \eqn{d_\min} is the smallest depth where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
+#' where \eqn{d_\max} is the maximum tree depth, \eqn{d_\min} is the minimum depth where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
 #'
 #' If \code{method = weighted_bonferroni}, the confidence level at depth d is
 #' \deqn{
-#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d + 2) n_d \sum_{B=2}^{d_\max - d_\min + 2}1/B},D\geq D_\min,
+#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d + 2) n_d \sum_{B=2}^{d_\max - d_\min + 2}1/B},D\geq D_\min.
 #' }
-#' that is, the leaves get weights alpha/2 etc.
 #'
-#' @param nd A vector. Number of bounded regions at every level.
-#' @param alpha Numeric. confidence level.
-#' @param method Use \code{bonferroni} or \code{weighted_bonferroni} method to adjust for multiple hypothesis testing.
-#' @return A vector of confidence level at every depth (starting at depth = 1)
+#'
+#' @param nd A vector. Number of bounded regions at every depth (starting at depth 0).
+#' @param alpha Numeric. The significance level (of the average density) of all the regions adds to \eqn{\alpha}.
+#' @param method Use \code{method = "bonferroni"} or \code{method = "weighted_bonferroni"} method to adjust for multiple hypothesis testing.
+#' @return A vector of significance level \eqn{\alpha_D} at every tree depth (starting at depth = 0)
 #' @export
 ConfLevel <- function(nd, alpha, method){
   Dmax <- length(nd)  # depth of tree
@@ -34,7 +34,7 @@ ConfLevel <- function(nd, alpha, method){
     )
   }
   if(Dmin>1) {  # dummy value: first few will not be used as there are no bounded regions
-    ahat[1:(Dmin-1)] <- 0.5
+    ahat[1:(Dmin-1)] <- 0
   }
 
   return(ahat)
