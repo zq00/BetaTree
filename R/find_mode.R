a4bce6c5f7a3fe98da9b784b2d10daef3b28f501
Qian Zhao <qzhao1@stanford.edu>
2023-09-11 15:24:10 -0700

    edit documentation

--- R/find_mode.R
+++ R/find_mode.R
@@ -1,24 +1,24 @@
-#' Identifies distinct modes in Beta trees histogram
+#' Identifies distinct modes in Beta-trees histogram
 #'
 #' Compute modes in the histogram.
 #'
 #' Two regions \code{i} and \code{j} are distinct modes if along every path connecting them,
 #'   there exists one region whose upper confidence limit is below the lower confidence limit
 #'   of both \code{i} and \code{j}.
-#' This function iterates through all the regions in the Beta tree histogram in
+#' This function iterates through all the regions in the Beta-tree histogram in
 #'   descending order of empirical density and marks a region as a mode if it is a distinct mode
 #'   from all currently discovered distinct modes.
 #'
 #' @param hist A matrix. Each row represents one region in the histogram.
 #'   If the histogram is constructed for \code{d} dimensional data, then the first \code{d} columns contain the lower bounds for each coordinate,
-#'   and the next \code{d} columns contain the upper bound. The \code{2d + 1} column contains the empirical density.
-#'    \code{hist} can be the output of \link[BetaTrees]{BuildHist} or \link[BetaTrees]{SelectNodes} function.
+#'   and the next \code{d} columns contain the upper bounds. The \code{2d + 1} column contains the empirical density.
+#'    \code{hist} can be the output of \link[BetaTree]{BuildHist} or \link[BetaTree]{SelectNodes} function.
 #' @param d A numeric value of the data dimension.
 #' @param cutoff Maximum path length. By default, we only evaluate paths with length at most \code{cutoff = 6}.
 #' @returns
 #' \describe{
 #' \item{mode}{A vector of candidate modes.}
-#' \item{hist}{The input Beta tree histogram.}
+#' \item{hist}{The input Beta-tree histogram.}
 #' \item{g}{A graph object constructed using the adjacency matrix of the regions in the histogram.}
 #' }
 #' @export
@@ -27,7 +27,7 @@
 #' # A mixture of two Gaussians
 #' X <-  matrix(rnorm(6000, 0, 1), ncol = 2)
 #' X[1:1500, 1] <- X[1:1500, 1] + 4
-#' rect <- BuildHist(X) # construct a Beta Tree histogram
+#' rect <- BuildHist(X) # construct a Beta-tree histogram
 #' modes <- FindModes(hist = rect, d = 2)$mode
 #' rect[modes, ] # show information of the modes
 
