d6eba75f46a436621904c5e94554b8c71b2e1324
Qian Zhao <qzhao1@stanford.edu>
2023-09-11 16:20:46 -0700

    update documentation and website

--- R/ConfLevel.R
+++ R/ConfLevel.R
@@ -1,12 +1,13 @@
-#' Set confidence level for regions at each tree depth
+#' Set significance level for regions at each tree depth
 #'
-#' Computes the confidence level at each tree depth.
+#' Computes the significance level at each tree depth.
 #'
+#' The root node automatically has significance level 0.
 #' If \code{method = "bonferroni"}, the significance level at depth d is
 #' \deqn{
 #' \hat{\alpha}_d =  \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, D\geq D_\min,
 #' }
-#' where \eqn{d_\max} is the maximum tree depth, \eqn{d_\min} is the minimum depth where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
+#' where \eqn{d_\max} is the maximum tree depth, \eqn{d_\min} is the minimum depth (greater than 0) where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
 #'
 #' If \code{method = weighted_bonferroni}, the confidence level at depth d is
 #' \deqn{
@@ -15,26 +16,23 @@
 #'
 #'
 #' @param nd A vector. Number of bounded regions at every depth (starting at depth 0).
-#' @param alpha Numeric. The significance level (of the average density) of all the regions adds to \eqn{\alpha}.
+#' @param alpha Numeric. The significance level (of the average density) of all the regions sums to \eqn{\alpha}.
 #' @param method Use \code{method = "bonferroni"} or \code{method = "weighted_bonferroni"} method to adjust for multiple hypothesis testing.
 #' @return A vector of significance level \eqn{\alpha_D} at every tree depth (starting at depth = 0)
 #' @export
 ConfLevel <- function(nd, alpha, method){
-  Dmax <- length(nd)  # depth of tree
-  Dmin <- min(which(nd > 0))
-  # set sig. level to each depth
-  ahat <- numeric(Dmax)
-  for(l in Dmin:Dmax){
+  DmaxP1 <- length(nd)   # depth of tree
+  DminP1 <- max(min(which(nd > 0)), 2)
+  # set sig. level to each depth (ahat[1] corresponds to tree depth 0, and has level = 0)
+  ahat <- numeric(DmaxP1)
+  for(l in DminP1:DmaxP1){
     switch (method,
-            "bonferroni" = {ahat[l] <- alpha / (Dmax - Dmin + 1) / nd[l]},
+            "bonferroni" = {ahat[l] <- alpha / (DmaxP1 - DminP1 + 1) / nd[l]},
             "weighted_bonferroni" = {
-              wsum <- sum(1/(2:(Dmax-Dmin + 2)))
-              ahat[l] <- alpha / (Dmax - l + 2) / nd[l] / wsum
+              wsum <- sum(1/(2:(DmaxP1-DminP1 + 2)))
+              ahat[l] <- alpha / (DmaxP1 - l + 2) / nd[l] / wsum
             }
     )
-  }
-  if(Dmin>1) {  # dummy value: first few will not be used as there are no bounded regions
-    ahat[1:(Dmin-1)] <- 0
   }
 
   return(ahat)
