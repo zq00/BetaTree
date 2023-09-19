4ebbf43ceb2c3ef887768c10845316a23623fa96
Qian Zhao <qzhao1@stanford.edu>
2023-09-19 15:52:04 -0700

    edit documentation

--- R/ConfLevel.R
+++ R/ConfLevel.R
@@ -5,13 +5,13 @@
 #' The root node automatically has significance level 0.
 #' If \code{method = "bonferroni"}, the significance level at depth d is
 #' \deqn{
-#' \hat{\alpha}_d =  \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, D\geq D_\min,
+#' \hat{\alpha}_d =  \frac{\alpha}{(d_\max - d_\min + 1) * n_d}, d\geq d_\min,
 #' }
 #' where \eqn{d_\max} is the maximum tree depth, \eqn{d_\min} is the minimum depth (greater than 0) where there are bounded regions, and \eqn{n_d} is number of bounded regions at depth \eqn{d}.
 #'
 #' If \code{method = weighted_bonferroni}, the confidence level at depth d is
 #' \deqn{
-#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d + 2) n_d \sum_{B=2}^{d_\max - d_\min + 2}1/B},D\geq D_\min.
+#' \hat{\alpha}_d = \frac{\alpha}{(d_\max - d + 2) n_d \sum_{B=2}^{d_\max - d_\min + 2}1/B},d\geq d_\min.
 #' }
 #'
 #'
@@ -21,8 +21,8 @@
 #' @return A vector of significance level \eqn{\alpha_D} at every tree depth (starting at depth = 0)
 #' @export
 ConfLevel <- function(nd, alpha, method){
-  DmaxP1 <- length(nd)   # depth of tree
-  DminP1 <- max(min(which(nd > 0)), 2)
+  DmaxP1 <- length(nd)   # tree depth + 1
+  DminP1 <- max(min(which(nd > 0)), 2) # (minimum tree depth where there are bounded regions) + 1; if the root node is bounded, then set to be 2
   # set sig. level to each depth (ahat[1] corresponds to tree depth 0, and has level = 0)
   ahat <- numeric(DmaxP1)
   for(l in DminP1:DmaxP1){
